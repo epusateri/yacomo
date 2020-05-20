92373e6dd3f7c172143184e53655fc995221bcc4
Ernest Pusateri <erniep@gmail.com>
2020-05-19 22:32:18 -0400

    0519 predictions

--- predictions/20200519/recipe.sh
+++ predictions/20200519/recipe.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# git rev-parse HEAD > hash
+git rev-parse HEAD > hash
 
-# curl -O https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv
+curl -O https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv
 
-# yacomo \
-#     --verbose \
-#     data \
-#     extract \
-#     --config-file data.yaml \
-#     --output-file data.json
+yacomo \
+    --verbose \
+    data \
+    extract \
+    --config-file data.yaml \
+    --output-file data.json
 
-# yacomo \
-#     --verbose \
-#     model \
-#     train \
-#     --data-file data.json \
-#     --config-file train.yaml \
-#     --predictor-file predictors.json
+yacomo \
+    --verbose \
+    model \
+    train \
+    --data-file data.json \
+    --config-file train.yaml \
+    --predictor-file predictors.json
 
-# yacomo \
-#     --verbose \
-#     model \
-#     predict \
-#     --predictor-file predictors.json \
-#     --num-days 200 \
-#     --predictions-file predictions.json
+yacomo \
+    --verbose \
+    model \
+    predict \
+    --predictor-file predictors.json \
+    --num-days 200 \
+    --predictions-file predictions.json
 
 yacomo \
     --verbose \
